341a8d8ce2f6819ea84c97580f2bdcb1ae3f0316
Duvan <duvanriano2626@gmail.com>
2024-08-14 11:45:38 -0500

    Endpoints para iniciar a hacer el CRUD

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
@@ -27,6 +27,27 @@ export class HeroesService {
 
   getSuggestions( query: string ): Observable<Hero[]> {
     return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
+  }
+
+
+  public addHero = (hero:Hero):Observable<Hero> => {
+    return this.http.post<Hero>(`${ this.baseUrl }/heroes`,hero);
+  }
+
+  public updateHero = (hero:Hero):Observable<Hero> => {
+    if (!hero.id) {
+      throw Error ('Hero id is required');
+    }
+    return this.http.patch<Hero>(`${ this.baseUrl }/heroes/${hero.id}`,hero);
+  }
+
+  public deleteHeroById = (id:string):Observable<boolean> => {
+
+    return this.http.delete(`${ this.baseUrl }/heroes/${id}`)
+      .pipe(
+        catchError(err => of(false)),
+        map(resp => true)
+      );
   }
 
 
