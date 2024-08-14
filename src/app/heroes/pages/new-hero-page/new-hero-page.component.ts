e3e2867570d2d2e885b040cf160cde1bad529882
Duvan <duvanriano2626@gmail.com>
2024-08-14 12:30:15 -0500

    Conectar el formulario con el template

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -31,4 +31,14 @@ export class NewHeroPageComponent {
     }
   ]
 
+
+
+  public onSubmit = (): void => {
+    console.log({
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    });
+
+  }
+
 }
