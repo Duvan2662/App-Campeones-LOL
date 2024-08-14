7a7f420e4165cfc46adf67f243aa37578f728280
Duvan <duvanriano2626@gmail.com>
2024-08-14 10:50:07 -0500

    Autocompletado con su funcionalidad

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,31 +1,34 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { catchError, Observable, of } from 'rxjs';
+
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroesService {
 
   private baseUrl: string = environments.baseUrl;
 
+
   constructor(private http: HttpClient) { }
 
 
-  getHeroes = (): Observable<Hero[]> => {
-    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`)
+  getHeroes():Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes`);
   }
 
-  getHeroById = (id:string): Observable<Hero | undefined> => {
-    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
-    .pipe(
-      catchError(error => of(undefined))
-    )
+  getHeroById( id: string ): Observable<Hero|undefined> {
+    return this.http.get<Hero>(`${ this.baseUrl }/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(undefined) )
+      );
   }
 
-  getSuggestions(query:string): Observable<Hero[]> {
-    return this.http.get<Hero[]>(`http://localhost:3000/heroes?=${query}&_limit=6`);
+  getSuggestions( query: string ): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${ this.baseUrl }/heroes?q=${ query }&_limit=6`);
   }
+
+
+
 }
