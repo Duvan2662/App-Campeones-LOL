34de39522b19f3b3ace5fdd1d19f181e1ff7d218
Duvan <duvanriano2626@gmail.com>
2024-08-14 13:41:49 -0500

    Establecer los valoresdel formulario cuando se va a editar un heroe

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-hero-page',
   templateUrl: './new-hero-page.component.html',
   styleUrl: './new-hero-page.component.css'
 })
-export class NewHeroPageComponent {
+export class NewHeroPageComponent implements OnInit{
 
 
   public heroForm = new FormGroup({
@@ -34,7 +36,29 @@ export class NewHeroPageComponent {
 
 
 
-  constructor (private heroesServices: HeroesService) {
+  constructor (
+    private heroesServices: HeroesService,
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) {
+  }
+
+  ngOnInit(): void {
+    if(!this.router.url.includes('edit')){
+      return
+    }
+
+    this.activateRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesServices.getHeroById(id)),
+      ).subscribe(hero =>{
+        if(!hero){
+          return this.router.navigateByUrl('/')
+        }
+
+        this.heroForm.reset(hero)
+        return
+      });
 
   }
 
