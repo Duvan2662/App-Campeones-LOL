dc419f81a247d7ec6671a6b0a5532b1f4c684b97
Duvan <duvanriano2626@gmail.com>
2024-08-15 14:17:42 -0500

    Creacion de guards en angular por si un usuario no autenticado intenta ingresar a una de mis rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { Page404Component } from './shared/pages/page404/page404.component';
+import { AuthGuard } from './auth/guards/auth.guard';
 
 const routes: Routes = [
 
@@ -12,6 +13,8 @@ const routes: Routes = [
   {
     path: 'heroes',
     loadChildren:() => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    canActivate:[AuthGuard],
+    canMatch:[AuthGuard]
   },
 
   {
