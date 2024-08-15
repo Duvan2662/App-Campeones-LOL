ccd2987082799ccb95ff7e1187109f1a99c081ec
Duvan <duvanriano2626@gmail.com>
2024-08-15 14:01:03 -0500

    Mantener la sesion del usuario

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -23,13 +23,27 @@ export class AuthService {
 
 
   public login = (email:string, password:string): Observable<User> => {
-
-
     return this.http.get<User>(`${this.baseUrl}/users/1`)
       .pipe(
         tap(user => this.user = user),
         tap(user => localStorage.setItem('token','assdasadd.asafASDF.ASADAD.')),
 
+      )
+  }
+
+
+
+  public checkAuthentication = (): Observable<boolean> => {
+    if(!localStorage.getItem('token')){
+      return of (false)
+    }
+
+    const token = localStorage.getItem('token');
+    return this.http.get<User>(`${this.baseUrl}/users/1`)
+      .pipe	(
+        tap(user => this.user = user),
+        map(user => !!user),
+        catchError(err => of(false))
       )
   }
 
